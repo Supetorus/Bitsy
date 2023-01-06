21e80afdbfddc33fd473b5ff6ed60252333a05d4
YukiSamavu <Gsomerville@student.neumont.edu>
2023-01-06 16:35:52 -0700

    Added in a few comments for monday

--- Assets/Raycast/Raycast.cs
+++ Assets/Raycast/Raycast.cs
@@ -12,10 +12,11 @@ public class Raycast : MonoBehaviour
   // Start is called before the first frame update
 
   private void Update() {
-    FrontPos = new Vector3(vision.transform.forward.x * 4.8f, vision.transform.forward.y, vision.transform.forward.z * 4.8f);
+    FrontPos = new Vector3(vision.transform.forward.x * 5, vision.transform.forward.y, vision.transform.forward.z * 5);
     //vision.transform.rotation = Quaternion.AngleAxis(10, Vector3.up);
     RightPos = new Vector3(vision.transform.right.x * 5, vision.transform.right.y, vision.transform.right.z * 5);
     LeftPos = new Vector3(vision.transform.right.x * -5, vision.transform.right.y, vision.transform.right.z * -5);
+    //Find out a way to get angles inbetween left and right for raycasting
     Debug.DrawRay(vision.transform.position, FrontPos, Color.white, 1.0f);
     Debug.DrawRay(vision.transform.position, RightPos, Color.red, 1.0f);
     Debug.DrawRay(vision.transform.position, LeftPos, Color.blue, 1.0f);
