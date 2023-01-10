52977ba5eaaf192d805cf54672cbd102a42284d7
Supetorus <littlewilliamthomas@gmail.com>
2023-01-09 17:43:38 -0700

    Upgrades ThirdPersonCameraController to use input system, and adds support for controller input.

--- Assets/Scripts/ThirdPersonCameraController.cs
+++ Assets/Scripts/ThirdPersonCameraController.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
-[ExecuteInEditMode]
+//[ExecuteInEditMode]
 public class ThirdPersonCameraController : MonoBehaviour
 {
 	public Transform target;
@@ -16,32 +18,60 @@ public class ThirdPersonCameraController : MonoBehaviour
 	public float maxPitch = 89.9f;
 	[Range(-90, 90)]
 	public float minPitch = -89.9f;
-	public float sensitivity = 1;
+	public float mouseSensitivity = 1;
+	public float stickSensitivity = 0.5f;
 	public Vector3 startDirection;
 	[Tooltip("Camera won't pass through objects on these layers.")]
 	public LayerMask ignoredLayers;
+	public bool invertMouseX = false;
+	public bool invertMouseY = false;
+	public bool invertStickX = false;
+	public bool invertStickY = false;
 
 	private new Camera camera;
+	private Vector2 input = Vector2.zero;
+	private float lastMouseTime = 0;
+	private float lastStickTime = 0;
 
 	private void Start()
 	{
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 		camera = GetComponent<Camera>();
+
+		CameraControls cc = new CameraControls();
+		cc.Default.Enable();
+		cc.Default.LookMouse.performed += LookMouse;
+		cc.Default.LookStick.performed += LookStick;
+		cc.Default.LookStick.canceled += CanceledStick;
+	}
+	private void LookMouse(InputAction.CallbackContext context)
+	{
+		input = context.ReadValue<Vector2>() * new Vector2(invertMouseX ? -1 : 1, invertMouseY ? -1 : 1) * mouseSensitivity;
+		lastMouseTime = Time.unscaledTime;
 	}
 
-	// Update is called once per frame
+	private void LookStick(InputAction.CallbackContext context)
+	{
+		input = context.ReadValue<Vector2>() * new Vector2(invertStickX ? -1 : 1, invertStickY ? -1 : 1) * stickSensitivity;
+		lastStickTime = Time.unscaledTime;
+	}
+
+	private void CanceledStick(InputAction.CallbackContext context)
+	{
+		input = Vector2.zero;
+	}
+
 	void Update()
 	{
-		yaw = (yaw + Input.GetAxis("Mouse X") * sensitivity) % 360;
-		pitch -= Input.GetAxis("Mouse Y") * sensitivity;
+		yaw = (yaw + input.x) % 360;
+		pitch -= input.y;
 		pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
 		Quaternion qYaw = Quaternion.AngleAxis(yaw, Vector3.up);
 		Quaternion qPitch = Quaternion.AngleAxis(pitch, Vector3.right);
 		Quaternion rotation = qYaw * qPitch;
 
 		RaycastHit hit;
-
 		Physics.SphereCast(target.position, camera.nearClipPlane, rotation * Vector3.back, out hit, distance, ignoredLayers);
 		if (hit.collider)
 		{
@@ -54,6 +84,10 @@ public class ThirdPersonCameraController : MonoBehaviour
 		}
 		transform.rotation = Quaternion.LookRotation(target.position - transform.position, Vector3.up);
 
+		if (lastMouseTime > lastStickTime) // player is using stick controls.
+		{
+			input = Vector2.zero;
+		}
 	}
 
 	private void OnDrawGizmosSelected()
