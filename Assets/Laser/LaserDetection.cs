8d1efa3c88029b3f417cdddbec00147e1678fe10
YukiSamavu <Gsomerville@student.neumont.edu>
2023-01-10 15:49:28 -0700

    Fixed spacing in the script

--- Assets/Laser/LaserDetection.cs
+++ Assets/Laser/LaserDetection.cs
@@ -6,15 +6,15 @@ public class LaserDetection : MonoBehaviour
 {
   public bool doesDamage;
 
-    private void OnCollisionEnter(Collision collision) {
+  private void OnCollisionEnter(Collision collision) {
     if (collision.gameObject.tag == "Player") {
       if (doesDamage) {
+        //Lower the players Health
         Debug.Log("You have taken damage");
       } else {
+        //Increase the detection meter
         Debug.Log("You Have Been Spotted");
       }
-    } else {
-      Debug.Log("Nothing to see here");
     }
-    }
+  }
 }
