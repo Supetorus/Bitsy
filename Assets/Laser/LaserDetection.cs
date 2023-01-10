7734d2d71d05f04aa81e7e338b4920430d4f7a86
YukiSamavu <Gsomerville@student.neumont.edu>
2023-01-10 16:06:03 -0700

    Changed to triggers and removed non player

--- Assets/Laser/LaserDetection.cs
+++ Assets/Laser/LaserDetection.cs
@@ -6,8 +6,8 @@ public class LaserDetection : MonoBehaviour
 {
   public bool doesDamage;
 
-  private void OnCollisionEnter(Collision collision) {
-    if (collision.gameObject.tag == "Player") {
+  private void OnTriggerEnter(Collider other) {
+    if (other.gameObject.tag == "Player") {
       if (doesDamage) {
         //Lower the players Health
         Debug.Log("You have taken damage");
