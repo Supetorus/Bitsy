c8a17143073f62b94fc3f62c6e314f3298d42b1e
Supetorus <littlewilliamthomas@gmail.com>
2023-01-19 08:24:35 -0700

    Fills out some basic fall state behavior.

--- Assets/Scripts/Movement/FallState.cs
+++ Assets/Scripts/Movement/FallState.cs
@@ -6,11 +6,25 @@ public class FallState : MovementState
 {
 	public override void EnterState()
 	{
-		c.move.action.Enable();
+		//c.move.action.Enable();
+		rigidbody.isKinematic = false;
+		rigidbody.useGravity = true;
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.angularVelocity = Vector3.zero;
+	}
+
+	public override void FixedUpdateState()
+	{
+		transform.rotation = Quaternion.LookRotation(transform.forward, Vector3.up); // Todo this should be a slerp from the original position to facing upright.
+
+		if (sd.GetClosestPoint(sd.groundCheckDistance) != null)
+		{
+			c.CurrentMovementState = c.clingState;
+		}
 	}
 
 	public override void ExitState()
 	{
-		c.move.action.Disable();
+		//c.move.action.Disable();
 	}
 }
