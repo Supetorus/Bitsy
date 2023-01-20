6168230270d5a5798f6f51cbe218adea477f29ab
Alayna Garry <77999316+AlaynaGarry@users.noreply.github.com>
2023-01-20 13:17:50 -0700

    Last Min Fixes

--- Assets/Scripts/Interaction/Hack.cs
+++ Assets/Scripts/Interaction/Hack.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Hack : MonoBehaviour, IInteractable {
   [SerializeField] private string _prompt;
+  [SerializeField] private TMP_Text _promptText;
 
   public string InteractPrompt => _prompt;
 
   public bool Interact(Interactor interactor) {
+        _promptText.text = InteractPrompt;
     Debug.Log(_prompt);
     return true;
   }
