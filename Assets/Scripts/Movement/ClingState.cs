2c1817ef966e36723cb2c3de15b973b526da217c
Supetorus <littlewilliamthomas@gmail.com>
2023-01-23 10:52:48 -0700

    Improves the player cling state movement.

--- Assets/Scripts/Movement/ClingState.cs
+++ Assets/Scripts/Movement/ClingState.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Processors;
 
 public class ClingState : MovementState
 {
-	[SerializeField, Tooltip("How fast the spider moves forward.")]
-	private float forwardSpeed = 1;
-	[SerializeField, Tooltip("How fast the spider moves backward.")]
-	private float backSpeed = 1;
-	[SerializeField, Tooltip("How fast the spider moves sideways.")]
-	private float sideSpeed = 1;
-	[SerializeField, Tooltip("This defines how far the spider keeps itself above a surface. Adjust this to make the legs line up with the ground. Must be less than the State Data's attachment distance.")]
+	[SerializeField, Tooltip("This defines how far the spider keeps itself above a surface. " +
+		"Adjust this to make the legs line up with the ground. Must be less than the State " +
+		"Data's attachment distance."), Min(0)]
 	private float height = 0.5f;
-	[SerializeField]
+	[SerializeField, Tooltip("The maximum lateral speed of the spider."), Min(0)]
 	private float maxVelocity = 1;
-	[SerializeField]
+	[SerializeField, Tooltip("Influences how quickly the spider gets to max velocity.")]
+	private float acceleration = 1;
+	[SerializeField, Tooltip("Influences how quickly the spider slows down when input stops." +
+		" Higher numbers apply less drag."), Range(0, 1)]
 	private float drag;
 
+	// The velocity of the spider, this is used to make the movement smoother so
+	// it accelerates and decelerates over time instead of instantly.
 	private Vector3 velocity;
 
 	public override void EnterState()
@@ -43,9 +45,9 @@ public class ClingState : MovementState
 
 	public override void FixedUpdateState()
 	{
-		if (height > sd.attachmentDistance)
+		if (height > sd.lesserAttachmentDistance)
 		{
-			Debug.Log("Cling State height cannot be greater than State Data's attachment distance.");
+			Debug.Log("Cling State height cannot be greater than State Data's lesser attachment distance.");
 			return;
 		}
 
@@ -57,38 +59,42 @@ public class ClingState : MovementState
 
 		Vector2 input = c.move.action.ReadValue<Vector2>();
 		Vector3? closestPoint = sd.GetClosestPoint(sd.attachmentDistance);
-		velocity *= drag;
+		if (input == Vector2.zero) velocity *= drag;
 
+		// Near a walkable surface
 		if (closestPoint != null)
 		{
 			// Movement
-			float distance = ((Vector3)(transform.position - closestPoint)).magnitude;
-			float x = input.x * sideSpeed;
-			float y = height - distance;
-			float z;
-			if (input.y > 0)
-			{
-				z = input.y * forwardSpeed;
-			}
-			else
-			{
-				z = input.y * backSpeed;
-			}
-			Vector3 direction = transform.rotation * new Vector3(x * Time.fixedDeltaTime, y, z * Time.fixedDeltaTime);
-			velocity = Vector3.ClampMagnitude(direction + velocity, maxVelocity);
-
+			// This is not normalized because it isn't required by the methods that use it.
+			Vector3 up = transform.position - (Vector3)closestPoint;
+			// distance from the center of object to the ground.
+			float distance = up.magnitude;
+			input = Vector3.ClampMagnitude(input, 1);
+			Vector3 direction = transform.rotation * new Vector3(
+				input.x * Time.deltaTime * acceleration,
+				height - distance,
+				input.y * Time.deltaTime * acceleration
+				);
+			velocity = Vector3.ClampMagnitude(velocity + direction, maxVelocity);
 			rigidbody.MovePosition(velocity + transform.position);
 
 			// Rotation
 			// https://discord.com/channels/489222168727519232/885300730104250418/1063576660051501136
-			Vector3 up = transform.position - (Vector3)closestPoint;
+			// Sets the forward direction of the spider based on camera.
 			Vector3 forward = Vector3.ProjectOnPlane(sd.camera.forward, up);
 			Quaternion targetRotation = Quaternion.LookRotation(forward, up);
-			Quaternion finalRotation = Quaternion.Slerp(transform.rotation, targetRotation, Quaternion.Angle(transform.rotation, targetRotation) / 360);
-			rigidbody.MoveRotation(finalRotation);
+			// Slerp is used to make the rotation more gradual so it doesn't instantly snap.
+			rigidbody.MoveRotation(
+				Quaternion.Slerp(
+					transform.rotation,
+					targetRotation,
+					Quaternion.Angle(transform.rotation, targetRotation) / 360
+					)
+				);
 		}
+		// Not near any walkable surfaces.
 		else
-		{ // Not near any walkable surfaces.
+		{
 			c.CurrentMovementState = c.fallState;
 		}
 	}
