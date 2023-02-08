9733f21f319db2f5270aca15a92bf47c1d938d0d
mcone880 <79989621+mcone880@users.noreply.github.com>
2023-02-08 14:51:55 -0700

    Turrets Working

--- Assets/Scripts/Enemies/Turret.cs
+++ Assets/Scripts/Enemies/Turret.cs
@@ -1,41 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Turret : MonoBehaviour
 {
-	[SerializeField] Animator animator;
-	private void OnEnable()
-	{
-		GlobalPlayerDetection.onThreeFourths += EnableTurret;
-		GlobalPlayerDetection.onHalf += DisableTurret;
-	}
+	[SerializeField] float sightDist;
+	[SerializeField] GameObject projectile;
+	[SerializeField] Transform weapon;
+	[SerializeField] LayerMask myMask;
+	[SerializeField] float projSpeed;
+	[SerializeField] float fireRate;
+	[HideInInspector] float fireTimer;
 
-	private void OnDisable()
-	{
-		GlobalPlayerDetection.onThreeFourths -= EnableTurret;
-		GlobalPlayerDetection.onHalf -= DisableTurret;
-	}
+	GameObject player;
+	TurretAnimator turretAnimator;
+	bool canSeePlayer;
 
-	// Start is called before the first frame update
-	void Start()
+    // Start is called before the first frame update
+    void Start()
     {
+		turretAnimator = GetComponentInParent<TurretAnimator>();
+		player = GameObject.FindGameObjectWithTag("Player");
+		fireTimer = fireRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-	}
-
-	void EnableTurret()
-	{
-		animator.SetBool("isActive", true);
-	}
-
-	void DisableTurret()
-	{
-		animator.SetBool("isActive", false);
-	}
+		Vector3 direction = (player.transform.position - transform.position).normalized;
+		Debug.DrawRay(transform.position, direction);
+		if (Physics.Raycast(transform.position, direction, out RaycastHit hit,  sightDist, myMask))
+		{
+			canSeePlayer = true;
+			if (hit.collider.gameObject == player)
+			{
+				turretAnimator.animator.enabled = false;
+				weapon.LookAt(player.transform);
+			}
+		}
+		else
+		{
+			canSeePlayer = false;
+			print("Animator on");
+			turretAnimator.animator.enabled = true;
+			turretAnimator.animator.SetBool("isActive", true);
+		}
+		
+		if (fireTimer <= 0 && canSeePlayer)
+		{
+			if (hit.collider.gameObject == player)
+			{
+				GameObject bullet = Instantiate(projectile, transform.position, transform.rotation);
+				bullet.GetComponent<Rigidbody>().AddForce(direction * projSpeed);
+				fireTimer = fireRate;
+			}
+		}
+		else
+		{
+			fireTimer -= Time.deltaTime;
+		}
+    }
 }
