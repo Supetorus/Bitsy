56d9e8377c5a50d541b9cfd9b2e5018e7501eb2c
mcone880 <79989621+mcone880@users.noreply.github.com>
2023-02-09 15:03:13 -0700

    Camera Detection

---
+++ Assets/Scripts/Enemies/CameraDetection.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDetection : MonoBehaviour
+{
+	[SerializeField] float sightDist;
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
