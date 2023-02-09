a07e4a2162c24755f76a8391039a40559621b6fa
mcone880 <79989621+mcone880@users.noreply.github.com>
2023-02-09 16:45:41 -0700

    Camera Detection and Turret Detection

--- Assets/Scripts/Enemies/Turret.cs
+++ Assets/Scripts/Enemies/Turret.cs
@@ -21,6 +21,8 @@ public class Turret : MonoBehaviour
 
 	private int currentSpawnLocation;
 
+	private Vector3 playerDir;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,20 +31,18 @@ public class Turret : MonoBehaviour
 		fireTimer = fireRate;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-		Vector3 direction = (player.transform.position - transform.position).normalized;
-		Debug.DrawRay(transform.position, direction);
-		print(Mathf.Abs(rotTimer - timeToRotate));
-		if (Physics.Raycast(transform.position, direction, out RaycastHit hit,  sightDist, myMask))
+	public bool CheckSightlines()
+	{
+		playerDir = (player.transform.position - transform.position).normalized;
+		if (Physics.Raycast(transform.position, playerDir, out RaycastHit hit, sightDist, myMask))
 		{
 			if (hit.collider.gameObject == player && player.GetComponent<AbilityController>().isVisible)
 			{
 				rotTimer += Time.deltaTime;
 				canSeePlayer = true;
 				turretAnimator.animator.enabled = false;
-				weapon.rotation = Quaternion.Slerp(weapon.rotation, Quaternion.LookRotation(direction, weapon.up), rotTimer/timeToRotate);
+				weapon.rotation = Quaternion.Slerp(weapon.rotation, Quaternion.LookRotation(playerDir, weapon.up), rotTimer / timeToRotate);
+				return true;
 			}
 		}
 		else
@@ -52,15 +52,22 @@ public class Turret : MonoBehaviour
 			turretAnimator.animator.enabled = true;
 			turretAnimator.animator.SetBool("isActive", true);
 		}
-		
-		if (fireTimer <= 0 && canSeePlayer)
+		return false;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (fireTimer <= 0 && canSeePlayer && CheckSightlines())
 		{
-			if (hit.collider.gameObject == player && player.GetComponent<AbilityController>().isVisible && Vector3.Dot(weapon.forward, direction) > 0.95f)
+			Physics.Raycast(transform.position, playerDir, out RaycastHit hit, sightDist, myMask);
+			if (hit.collider.gameObject == player && player.GetComponent<AbilityController>().isVisible && Vector3.Dot(weapon.forward, playerDir) > 0.95f)
 			{
 				GameObject bullet = Instantiate(projectile, spawnLocations[currentSpawnLocation].position, transform.rotation);
-				bullet.transform.rotation = Quaternion.LookRotation(direction);
+				bullet.transform.rotation = Quaternion.LookRotation(playerDir);
 				bullet.GetComponent<Rigidbody>().AddForce((player.transform.position - spawnLocations[currentSpawnLocation].position).normalized * projSpeed);
 				Destroy(bullet, 1);
+				player.GetComponent<GlobalPlayerDetection>().ChangeDetection(0.25f, true);
 				currentSpawnLocation = (currentSpawnLocation + 1) % spawnLocations.Length;
 				fireTimer = fireRate;
 			}
@@ -70,9 +77,4 @@ public class Turret : MonoBehaviour
 			fireTimer -= Time.deltaTime;
 		}
     }
-
-	private void OnDrawGizmosSelected()
-	{
-		//Gizmos.DrawSphere(transform.position, sightDist);
-	}
 }
