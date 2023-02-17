09ae443ebbadc20b421974e2c317056388a54fa9
mcone880 <79989621+mcone880@users.noreply.github.com>
2023-02-17 14:43:15 -0700

    Upgrade UI Changes

---
+++ Assets/PlayerPrefSetup.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		//Misc Upgrades
+		PlayerPrefs.SetString("Lunge", "False");
+		PlayerPrefs.SetString("WebZip", "False");
+
+		//Abiity Upgrades
+		PlayerPrefs.SetString("SB_DU", "False");
+		PlayerPrefs.SetString("EMP_DU", "False");
+		PlayerPrefs.SetString("EMP_RU", "False");
+		PlayerPrefs.SetString("TD_U", "False");
+		PlayerPrefs.SetString("C_DU", "False");
+
+		PlayerPrefs.SetInt("SB_DURATION", 1);
+		PlayerPrefs.SetInt("EMP_DURATION", 1);
+		PlayerPrefs.SetInt("EMP_RADIUS", 1);
+		PlayerPrefs.SetString("TD_PENETRATION", "FALSE");
+		PlayerPrefs.SetInt("C_DURATION", 1);
+
+		//Ammo Upgrades
+		PlayerPrefs.SetInt("SB_AM", 1);
+		PlayerPrefs.SetInt("EMP_AM", 1);
+		PlayerPrefs.SetInt("TD_AM", 1);
+	}
+}
