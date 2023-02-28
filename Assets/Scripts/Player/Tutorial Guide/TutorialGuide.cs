a8f066e14d9c8d7c22b03d8b67987012a1340202
David-at-school <dsouthern@student.neumont.edu>
2023-02-28 15:48:02 -0700

    Comments unused PlasticSCM using

--- Assets/Scripts/Player/Tutorial Guide/TutorialGuide.cs
+++ Assets/Scripts/Player/Tutorial Guide/TutorialGuide.cs
@@ -2,7 +2,7 @@ using Michsky.UI.Reach;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.PlasticSCM.Editor.WebApi;
+//using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class TutorialGuide : MonoBehaviour
