7dec8b9d85d2c60a9ece4711dbabf261cd16c3b1
mcone880 <79989621+mcone880@users.noreply.github.com>
2023-03-08 15:19:59 -0700

    Lasers see invis player Fix

--- Assets/Scripts/General/HidingPlace.cs
+++ Assets/Scripts/General/HidingPlace.cs
@@ -6,10 +6,12 @@ public class HidingPlace : MonoBehaviour
 {
 	private void OnTriggerEnter(Collider other)
 	{
-		if (other.TryGetComponent(out AbilityController controller)) controller.isVisible = false;
+		print("NOW HIDE");
+		if (other.TryGetComponent(out AbilityController controller)) controller.isHiding = true;
 	}
 	private void OnTriggerExit(Collider other)
 	{
-		if (other.TryGetComponent(out AbilityController controller)) controller.isVisible = true;
+		print("BYE BYE");
+		if (other.TryGetComponent(out AbilityController controller)) controller.isHiding = false;
 	}
 }
